7436d9d08c60daa4c55c5fa7cc80644b08e421d5
Daniel Eynis <daniel.eynis@gmail.com>
2017-04-19 21:50:10 -0700

    Changed implementation to use a HashSet to store game pieces. The HashSet stores the position on pieces in a string in the form 'RowColumn'. Create a function to generate possible moves for piees on the board

--- src/Hexapawn.java
+++ src/Hexapawn.java
@@ -5,9 +5,12 @@ E-mail: eynis@pdx.edu
 Online resources consulted:
 https://docs.oracle.com/javase/7/docs/api/java/util/ArrayList.html
 https://docs.oracle.com/javase/7/docs/api/java/util/Scanner.html
+http://stackoverflow.com/questions/13942701/take-a-char-input-from-the-scanner
+http://stackoverflow.com/questions/17606839/creating-a-set-of-arrays-in-java
  */
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Hexapawn {
@@ -15,8 +18,8 @@ public class Hexapawn {
     private char currentTurn = 'W';
     private char[][] board;
     private int cols, rows;
-    private ArrayList<Pawn> whitePawns = new ArrayList<>();
-    private ArrayList<Pawn> blackPawns = new ArrayList<>();
+    private HashSet<String> wPawns = new HashSet<>();
+    private HashSet<String> bPawns = new HashSet<>();
 
     Hexapawn() {
         Scanner input = new Scanner(System.in);
@@ -24,6 +27,7 @@ public class Hexapawn {
         assert currentTurn == 'W' || currentTurn == 'B';
 
         ArrayList<String> inputLines = new ArrayList<>();
+
         String lineRead;
         while(input.hasNextLine()) {
             lineRead = input.nextLine();
@@ -32,34 +36,83 @@ public class Hexapawn {
             inputLines.add(lineRead);
             ++rows;
         }
-        assert inputLines.size() != 0 && rows != 0 && inputLines.size() == rows;
 
+        assert inputLines.size() != 0 && rows != 0 && inputLines.size() == rows;
         board = new char[rows][];
 
         char[] pieceArray;
         cols = inputLines.get(0).length();
-        for (int i = 0; i < inputLines.size(); ++i) {
+        for (int i = 0; i < inputLines.size(); ++i){
             pieceArray = inputLines.get(i).toCharArray();
             assert pieceArray.length == cols;
             board[i] = pieceArray;
-            for (int j = 0; j < pieceArray.length; ++j) {
+
+            for (int j = 0; j < pieceArray.length; ++j){
                 char curPos = pieceArray[j];
                 assert curPos == '.' || curPos == 'P' || curPos == 'p';
+
                 if(curPos == '.')
                     continue;
-                if(curPos == 'P') {
+                if(curPos == 'P'){
                     assert i != 0; // white pawn cannot start on opponent side
-                    whitePawns.add(new Pawn(i, j, 'P'));
+                    wPawns.add(i + "" + j);
                 }
-                else {
+                else{
                     assert i != rows-1;  // black pawn cannot start on opponent side
-                    blackPawns.add(new Pawn(i, j, 'p'));
+                    bPawns.add(i + "" + j);
                 }
             }
         }
+        assert wPawns.size() <= cols;
+        assert bPawns.size() <= cols;
+    }
 
-        assert whitePawns.size() <= cols;
-        assert blackPawns.size() <= cols;
+    public int solveBoard(){
+        return 0;
+    }
+
+    public int solveBoard(HashSet<String> wPawns, HashSet<String> bPawns){
+        return 0;
+    }
+
+    private ArrayList<int[]> generateMoves(HashSet<String> wPawns, HashSet<String> bPawns){
+        ArrayList<int[]> moves = new ArrayList<>();
+        int row, col;
+        HashSet<String> onMovePawns;
+
+        if(currentTurn == 'W')
+            onMovePawns = wPawns;
+        else
+            onMovePawns = bPawns;
+
+        for(String pos : onMovePawns){
+            row = Character.getNumericValue(pos.charAt(0));
+            col = Character.getNumericValue(pos.charAt(1));
+
+            if(currentTurn == 'W'){
+                if(!wPawns.contains((row-1) + "" + col)){
+                    moves.add(new int[]{row, col, row-1, col});
+                }
+                if(bPawns.contains((row-1) + "" + (col-1))){
+                    moves.add(new int[]{row, col, row-1, col-1});
+                }
+                if(bPawns.contains((row-1) + "" + (col+1))){
+                    moves.add(new int[]{row, col, row-1, col-1});
+                }
+            }
+            else{
+                if(!wPawns.contains((row+1) + "" + col)){
+                    moves.add(new int[]{row, col, row-1, col});
+                }
+                if(wPawns.contains((row+1) + "" + (col-1))){
+                    moves.add(new int[]{row, col, row-1, col-1});
+                }
+                if(wPawns.contains((row+1) + "" + (col+1))){
+                    moves.add(new int[]{row, col, row-1, col-1});
+                }
+            }
+        }
+        return moves;
     }
 
     private void printBoard(char[][] boardToPrint){
