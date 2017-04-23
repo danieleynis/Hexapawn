57f2cc5065f8d73aab485b150f64f50392898f29
Daniel Eynis <daniel.eynis@gmail.com>
2017-04-23 13:40:06 -0700

    added comments to explain

--- src/Hexapawn.java
+++ src/Hexapawn.java
@@ -16,22 +16,22 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class Hexapawn {
-    private char currentTurn;
+    private char currentTurn; // keeps track of the current player turn 'W' or 'B'
     private int cols, rows;
-    private HashSet<String> wPawns = new HashSet<>();
-    private HashSet<String> bPawns = new HashSet<>();
+    private HashSet<String> wPawns = new HashSet<>(); // hash set of all white pawns
+    private HashSet<String> bPawns = new HashSet<>(); // hash set of all black pawns
 
     Hexapawn() {
         Scanner input = new Scanner(System.in);
         currentTurn = input.nextLine().charAt(0);
         assert currentTurn == 'W' || currentTurn == 'B';
 
-        ArrayList<String> inputLines = new ArrayList<>();
+        ArrayList<String> inputLines = new ArrayList<>();  // read in input lines into an array list
 
         String lineRead;
         while(input.hasNextLine()) {
             lineRead = input.nextLine();
-            if(lineRead.isEmpty())
+            if(lineRead.isEmpty())  // one encounter empty line it is end of input, used for console input
                 break;
             inputLines.add(lineRead);
             ++rows;
@@ -40,7 +40,7 @@ public class Hexapawn {
         assert inputLines.size() != 0 && rows != 0 && inputLines.size() == rows;
 
         char[] pieceArray;
-        cols = inputLines.get(0).length();
+        cols = inputLines.get(0).length();  // get the number of columns
         for (int i = 0; i < inputLines.size(); ++i){
             pieceArray = inputLines.get(i).toCharArray();
             assert pieceArray.length == cols;
@@ -53,7 +53,7 @@ public class Hexapawn {
                     continue;
                 if(curPos == 'P'){
                     assert i != 0; // white pawn cannot start on opponent side
-                    wPawns.add(i + "" + j);
+                    wPawns.add(i + "" + j);  // add pawn position in format "rowcol" to hash set
                 }
                 else{
                     assert i != rows-1;  // black pawn cannot start on opponent side
@@ -70,39 +70,44 @@ public class Hexapawn {
     }
 
     private int solveBoard(HashSet<String> wPawns, HashSet<String> bPawns){
-        ArrayList<int[]> moves = generateMoves(wPawns, bPawns);
+        ArrayList<int[]> moves = generateMoves(wPawns, bPawns);  // get list of moves
 
-        if(moves == null || moves.size() == 0)
+        if(moves == null || moves.size() == 0)  // if no moves left, signifies a loss for side on move
             return -1;
 
         int max = -1;
         int val;
         for(int[] move : moves){
-            HashSet<String> copyPawnsW = new HashSet<>(wPawns);
+            HashSet<String> copyPawnsW = new HashSet<>(wPawns);  // create copy of white and black pawn hash sets
             HashSet<String> copyPawnsB = new HashSet<>(bPawns);
 
-            boolean win = executeMove(copyPawnsW, copyPawnsB, move);
+            boolean win = executeMove(copyPawnsW, copyPawnsB, move);  // execute move on copies
 
-            if(win)
+            if(win) // if execute move function returns true it is a win, return 1 no use in finding other wins
                 return 1;
 
-            currentTurn = (currentTurn == 'W' ? 'B' : 'W');
-            val = - solveBoard(copyPawnsW, copyPawnsB);
-            currentTurn = (currentTurn == 'W' ? 'B' : 'W');
+            currentTurn = (currentTurn == 'W' ? 'B' : 'W');  // flip the current turn before recursive call
+            val = - solveBoard(copyPawnsW, copyPawnsB);  // negate the return value of the recursive call (negamax)
+            currentTurn = (currentTurn == 'W' ? 'B' : 'W');  // flip back on recursive return
 
             max = Math.max(max, val);
         }
         return max;
     }
 
+    /*
+       This function will take hash sets of white and black pawns and an int array of size 4 of a move which
+       gives the starting position row and column and the position to move to row and column. This function will
+       modify the hash sets directly as it assumes copies are saved if necessary.
+     */
     private boolean executeMove(HashSet<String> wPawns, HashSet<String> bPawns, int[] move){
-        if(move[2] == 0 || move[2] == rows-1)
+        if(move[2] == 0 || move[2] == rows-1)  // if there is a winning move (pawn reached end) return true
             return true;
 
         HashSet<String> onMovePawns;
         HashSet<String> waitingPawns;
 
-        if(currentTurn == 'W') {
+        if(currentTurn == 'W') {  // figure out who is on move and who is waiting
             onMovePawns = wPawns;
             waitingPawns = bPawns;
         }
@@ -114,16 +119,21 @@ public class Hexapawn {
         String startLoc = move[0] + "" + move[1];
         String endLoc = move[2] + "" + move[3];
 
-        if(waitingPawns.contains(endLoc)){
+        if(waitingPawns.contains(endLoc)){  // if the position to move to has an opponent piece remove it
             waitingPawns.remove(endLoc);
         }
 
-        onMovePawns.remove(startLoc);
-        onMovePawns.add(endLoc);
+        onMovePawns.remove(startLoc);  // remove the old position for on move side
+        onMovePawns.add(endLoc);  // add the new position for on move side
 
-        return false;
+        return false;  // return no win
     }
 
+    /*
+       This function will generate a list of all possible moves for the side that is on move. An ArrayList of
+       int arrays will be returned specifying the starting position coordinated and the coordinates of position
+       to move to.
+     */
     private ArrayList<int[]> generateMoves(HashSet<String> wPawns, HashSet<String> bPawns){
         ArrayList<int[]> moves = new ArrayList<>();
         int row, col;
@@ -134,16 +144,18 @@ public class Hexapawn {
         else
             onMovePawns = bPawns;
 
-        for(String pos : onMovePawns){
-            row = Character.getNumericValue(pos.charAt(0));
+        for(String pos : onMovePawns){  // for each pawn for the side on move
+            row = Character.getNumericValue(pos.charAt(0));  // get the pawn exact coordinates
             col = Character.getNumericValue(pos.charAt(1));
 
             if(currentTurn == 'W'){
-                if(!bPawns.contains((row-1) + "" + col)){
+                if(!bPawns.contains((row-1) + "" + col)){  // if the opposing side is not blocking forward move
+                    // add move to beginning of ArrayList as checking moves forward speeds up the potential of finding
+                    // a winning move
                     moves.add(0, new int[]{row, col, row-1, col});
                 }
-                if(bPawns.contains((row-1) + "" + (col-1))){
-                    moves.add(new int[]{row, col, row-1, col-1});
+                if(bPawns.contains((row-1) + "" + (col-1))){  // if there is an opponent pawn across from the pawn
+                    moves.add(new int[]{row, col, row-1, col-1});  // add a move to attack that pawn
                 }
                 if(bPawns.contains((row-1) + "" + (col+1))){
                     moves.add(new int[]{row, col, row-1, col+1});
@@ -161,6 +173,6 @@ public class Hexapawn {
                 }
             }
         }
-        return moves;
+        return moves;  // return the list of moves
     }
 }
